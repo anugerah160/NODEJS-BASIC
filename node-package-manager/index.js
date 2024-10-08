5ba772b8cdc43211ee8bfcf2a8203ee620a4bf74
anugerah <157374618+anugerah160@users.noreply.github.com>
2024-10-08 19:15:59 +0700

    moment

--- node-package-manager/index.js
+++ node-package-manager/index.js
@@ -1,5 +1,8 @@
 const _ = require('lodash');
- 
+const moment = require('moment');
+
+const date = moment().format("MMM Do YY");
+console.log(date);
 const myOddEvenArray = _.partition([1, 2, 3, 4, 5, 6], (n) => n % 2);
  
 console.log(myOddEvenArray);
